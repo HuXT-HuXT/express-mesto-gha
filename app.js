302bd781d78e4cb9a50623d4b5c27f6f61aec3c3
Konstantin Suvorov <huxt-huxt@yandex.ru>
2022-10-01 19:43:56 +0300

    14PR_1st_fix

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 
-app.all('*', auth, function(next) {
+app.all('*', auth, (next) => {
   next(new NotFound('404! Страница не найдена.'));
 });
 
